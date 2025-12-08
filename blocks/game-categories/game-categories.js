0872a416f5b6bbfa9b939a350844573bae4253da
Kiran Lingadalli <kiran.lingadalli@hashouttech.com>
2025-12-08 18:02:15 +0530

    added game categories block css styles and js changes

--- blocks/game-categories/game-categories.js
+++ blocks/game-categories/game-categories.js
@@ -1,40 +1,3 @@
-// import { CreateElem } from '../../scripts/utils.js';
-
-// export default function decorate(block) {
-//   const allDivs = [...block.children];
-
-//   const titleDiv = allDivs[0];
-//   titleDiv.classList.add('game-category-title');
-
-//   const instructionsWrapper = CreateElem('div', 'game-instructions-list');
-
-//   allDivs.slice(1).forEach((item) => {
-//     const instructionItem = CreateElem('div', 'game-instruction-item');
-//     const innerDivs = [...item.children];
-
-//     const imageContainerDiv = innerDivs[0];
-//     const altTextDiv = innerDivs[1];
-//     const descriptionDiv = innerDivs[2];
-
-//     const picture = imageContainerDiv.querySelector('picture');
-//     const img = picture ? picture.querySelector('img') : imageContainerDiv.querySelector('img');
-
-//     const altText = altTextDiv?.querySelector('p')?.textContent?.trim() || '';
-//     if (img && altText) {
-//       img.setAttribute('alt', altText);
-//     }
-
-//     if (picture) picture.replaceWith(img);
-//     altTextDiv.remove();
-//     imageContainerDiv.className = 'game-instruction-item-image';
-
-//     descriptionDiv.className = 'game-instruction-item-description';
-//     instructionItem.append(imageContainerDiv, descriptionDiv);
-//     instructionsWrapper.append(instructionItem);
-//   });
-
-//   block.append(instructionsWrapper);
-// }
 import { CreateElem } from '../../scripts/utils.js';
 
 export default function decorate(block) {
@@ -76,4 +39,15 @@ export default function decorate(block) {
   });
 
   block.append(instructionsWrapper);
+
+  const buttonContainer = CreateElem('div', 'game-actions');
+  const createRoomBtn = CreateElem('a', 'game-action-button create-room-btn', 'create-room-btn', 'Create Room');
+  createRoomBtn.setAttribute('href', '#');
+
+  const joinRoomBtn = CreateElem('a', 'game-action-button join-room-btn', 'join-room-btn', 'Join Room');
+  joinRoomBtn.setAttribute('href', '#');
+
+  buttonContainer.append(createRoomBtn, joinRoomBtn);
+
+  block.append(buttonContainer);
 }
