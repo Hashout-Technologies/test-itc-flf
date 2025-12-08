d7e98414afcd411edd23c69f702e825442feb1c6
madukara08 <madukara.devadiga@hashouttech.com>
2025-12-08 18:41:02 +0530

    Update categories fetch URL in GameCategoryManager

--- components/game/game-categories.js
+++ components/game/game-categories.js
@@ -21,7 +21,7 @@ export class GameCategoryManager {
    */
   async fetchCategories() {
     try {
-      const response = await fetch('../../blocks/games/categories.json');
+      const response = await fetch('/game-questions.json');
       if (!response.ok) throw new Error('Failed to fetch');
       
       const data = await response.json();
